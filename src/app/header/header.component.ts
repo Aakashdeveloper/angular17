f18a276a356b1c1d9311d4af51061c762bec433d
Bhumika Bajpai <bhumikabajpai@Bhumikas-MacBook-Air.local>
2024-03-24 19:24:12 +0530

    angular17

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  encapsulation: ViewEncapsulation.None
+})
+export default class HeaderComponent {
+
+}
